34bb5e8033e95afd61285244b46b000d582cd3f7
Nick <nickbhenderson95@gmail.com>
2024-05-19 13:19:38 -0500

    Add files via upload
    Refactored the code slightly and changed some styles around a little.

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const dadButton = document.querySelector("#dadButton");
-dadButton.addEventListener("click", () => getDadJoke())
 
-const getDadJoke = async () => {
+const createDadJoke = async () => {
+    const dadJoke = await getDadJoke()
     const dadBox = document.querySelector("#dadBox");
-    const config = {headers: {Accept: "application/json"}}
-    const res = await axios.get("https://icanhazdadjoke.com/", config);
-    const dadJoke = res.data.joke;
     dadBox.innerHTML = dadJoke;
     const randomColor = () => Math.floor(Math.random() * 256)
     dadBox.style.color = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`
+}
+dadButton.addEventListener("click", () => createDadJoke())
+
+const getDadJoke = async () => {
+    const config = {headers: {Accept: "application/json"}}
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
     console.log(res.data)
+    return res.data.joke;
 }
